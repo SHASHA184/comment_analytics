f5b0c0d07279fc03b0eac6d81075664d859dc4f9
SHASHA184 <sashashelehan@gmail.com>
2024-08-22 00:32:42 +0300

    first commit

---
+++ database/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DEFAULT_USER')}:" \
+    f"{os.getenv('DEFAULT_PASSWORD')}@" \
+    f"{os.getenv('DEFAULT_HOST')}:{os.getenv('DEFAULT_PORT')}/" \
+    f"{os.getenv('DEFAULT_DB')}"
+
+GOOGLE_AI_API_KEY = os.getenv('GOOGLE_AI_API_KEY')
